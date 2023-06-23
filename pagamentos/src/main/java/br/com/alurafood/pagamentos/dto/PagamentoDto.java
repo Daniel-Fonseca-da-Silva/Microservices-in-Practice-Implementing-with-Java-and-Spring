b182bda64dcbbccfa476b5c66acfdec305473ee1
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-23 17:59:02 -0300

    feat: Created the dto of pagamento

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
@@ -0,0 +1,23 @@
+package br.com.alurafood.pagamentos.dto;
+
+import java.math.BigDecimal;
+
+import br.com.alurafood.pagamentos.model.Status;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PagamentoDto {
+	
+	private Long id;
+	private BigDecimal valor;
+	private String nome;
+	private String numero;
+	private String expiracao;
+	private String codigo;
+	private Status status;
+	private Long formaDePagamentoId;
+	private Long pedidoId;
+
+}
