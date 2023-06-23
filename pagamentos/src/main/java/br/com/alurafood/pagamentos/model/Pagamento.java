bf3df549afc634c3a014b8f6f3d5974a41844e83
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-23 17:58:09 -0300

    feat: Created the pagamento

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -0,0 +1,57 @@
+package br.com.alurafood.pagamentos.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pagamentos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pagamento {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Positive
+	private BigDecimal valor;
+	
+	@Size(max=100)
+	private String nome;
+	
+	@Size(max=19)
+	private String numero;
+	
+	@Size(max=7)
+	private String expiracao;
+	
+	@Size(min=3, max=3)
+	private String codigo;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private Status status;
+	
+	@NotNull
+	private Long pedidoId;
+	
+	@NotNull
+	private Long formaDePagamentoId;
+}
