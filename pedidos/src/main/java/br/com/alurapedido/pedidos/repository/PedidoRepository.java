67eea2d714314163584fb6859a5b0dfa98747397
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-23 18:07:07 -0300

    feat: Created the repository of pedido

---
+++ pedidos/src/main/java/br/com/alurapedido/pedidos/repository/PedidoRepository.java
@@ -0,0 +1,18 @@
+package br.com.alurapedido.pedidos.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.alurapedido.pedidos.model.Pedido;
+import br.com.alurapedido.pedidos.model.Status;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+	@Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Pedido p set p.status = :status where p = :pedido")
+    void atualizaStatus(Status status, Pedido pedido);
+
+    @Query(value = "SELECT p from Pedido p LEFT JOIN FETCH p.itens where p.id = :id")
+    Pedido porIdComItens(Long id);
+}
