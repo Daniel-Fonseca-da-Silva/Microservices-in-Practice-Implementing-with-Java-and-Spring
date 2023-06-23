08bc897bd5ece57b6eb0368ff67d973ac493cc09
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-23 18:08:48 -0300

    feat: Added another files missing inside pedidos

---
+++ pedidos/src/main/java/br/com/alurapedido/pedidos/PedidosApplication.java
@@ -0,0 +1,14 @@
+package br.com.alurapedido.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosApplication.class, args);
+	}
+}
